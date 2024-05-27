760a3c4c9d86d6cf8605865690a2664e3e42292a
Ibrahimyousuf22 <92476951+Ronaldodjd@users.noreply.github.com>
2024-05-27 19:11:29 -0400

    top points/goals

--- lib/account_screen.dart
+++ lib/account_screen.dart
@@ -1,239 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-  bool _isLoading = false;
-  String _errorMessage = '';
-
-  Future<void> _login() async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = '';
-    });
-    try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-
-      User? user = userCredential.user;
-
-      if (user != null) {
-        final database = FirebaseDatabase.instance.ref();
-        DatabaseEvent event = await database
-            .child('users')
-            .child(user.uid)
-            .child('formation')
-            .once();
-        DataSnapshot snapshot = event.snapshot;
-
-        String formation = snapshot.value as String? ?? '4231';
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setString('selectedFormation', formation);
-
-        // Navigate to the main app
-        Navigator.pushReplacementNamed(context, '/main');
-      } else {
-        setState(() {
-          _errorMessage = 'User is null';
-        });
-      }
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Login successful!'),
-      ));
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _errorMessage = e.message!;
-      });
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  Future<void> _register() async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = '';
-    });
-    try {
-      UserCredential userCredential =
-          await _auth.createUserWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Registration successful!'),
-      ));
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _errorMessage = e.message!;
-      });
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  Future<void> _signInWithGoogle() async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = '';
-    });
-    try {
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-
-      if (googleUser == null) {
-        setState(() {
-          _errorMessage = 'Google sign-in cancelled';
-        });
-        return;
-      }
-
-      final GoogleSignInAuthentication? googleAuth =
-          await googleUser.authentication;
-
-      if (googleAuth == null) {
-        setState(() {
-          _errorMessage = 'Google authentication failed';
-        });
-        return;
-      }
-
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      UserCredential userCredential =
-          await _auth.signInWithCredential(credential);
-
-      User? user = userCredential.user;
-
-      if (user != null) {
-        final database = FirebaseDatabase.instance.ref();
-        DatabaseEvent event = await database
-            .child('users')
-            .child(user.uid)
-            .child('formation')
-            .once();
-        DataSnapshot snapshot = event.snapshot;
-
-        String formation = snapshot.value as String? ?? '4231';
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setString('selectedFormation', formation);
-
-        // Navigate to the main app
-        Navigator.pushReplacementNamed(context, '/main');
-      } else {
-        setState(() {
-          _errorMessage = 'User is null';
-        });
-      }
-
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Google Sign-In successful!'),
-      ));
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _errorMessage = e.message!;
-      });
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  Future<void> _saveFormation(String formation) async {
-    User? user = _auth.currentUser;
-    if (user != null) {
-      final database = FirebaseDatabase.instance.ref();
-      await database
-          .child('users')
-          .child(user.uid)
-          .child('formation')
-          .set(formation);
-    }
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('selectedFormation', formation);
-  }
-
+class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text('Account Details'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
-              ),
+            user != null
+                ? CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                        user.photoURL ?? 'https://via.placeholder.com/150'),
+                  )
+                : Container(),
+            SizedBox(height: 20),
+            Text(
+              'Name: ${user?.displayName ?? 'N/A'}',
+              style: TextStyle(fontSize: 18),
             ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
-              ),
+            SizedBox(height: 10),
+            Text(
+              'Email: ${user?.email ?? 'N/A'}',
+              style: TextStyle(fontSize: 18),
             ),
-            SizedBox(height: 20.0),
-            if (_errorMessage.isNotEmpty)
-              Text(
-                _errorMessage,
-                style: TextStyle(color: Colors.red),
-              ),
-            SizedBox(height: 20.0),
-            if (_isLoading)
-              CircularProgressIndicator()
-            else
-              Column(
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: _login,
-                    icon: Icon(Icons.login),
-                    label: Text('Login'),
-                  ),
-                  SizedBox(height: 10.0),
-                  ElevatedButton.icon(
-                    onPressed: _register,
-                    icon: Icon(Icons.person_add),
-                    label: Text('Register'),
-                  ),
-                  SizedBox(height: 10.0),
-                  OutlinedButton.icon(
-                    onPressed: _signInWithGoogle,
-                    icon: Icon(Icons.golf_course),
-                    label: Text('Sign in with Google'),
-                  ),
-                ],
-              ),
+            SizedBox(height: 10),
+            Text(
+              'UID: ${user?.uid ?? 'N/A'}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushReplacementNamed('/login');
+              },
+              child: Text('Log Out'),
+            ),
           ],
         ),
       ),
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,13 @@
-import 'edit_team_page.dart';
-import 'more_matches_page.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert'; // Add this for JSON decoding
 
-class MainScreen extends StatelessWidget {
+import 'package:euros24fantasy/edit_team_page.dart';
+import 'package:euros24fantasy/more_matches_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:http/http.dart' as http; // Add this for HTTP requests
+import 'player.dart'; // Assuming Player class is defined in player.dart
+
+class MainScreen extends StatefulWidget {
   final String htmlContent;
   final int userPoints;
   final String countdown;
@@ -13,6 +18,103 @@ class MainScreen extends StatelessWidget {
     required this.userPoints,
     required this.countdown,
   }) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<Player> topScorers = [];
+  List<Player> topPoints = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTopScorers();
+    fetchTopPoints();
+  }
+
+  Future<void> fetchTopScorers() async {
+    final DatabaseReference _databaseReference =
+        FirebaseDatabase.instance.reference();
+
+    try {
+      final DatabaseEvent event = await _databaseReference.once();
+      final snapshot = event.snapshot;
+
+      if (snapshot.value != null) {
+        final data = snapshot.value as Map<dynamic, dynamic>;
+        final List<dynamic> playersData = data.values.toList();
+
+        List<Player> players = playersData.map((item) {
+          return Player(
+            playerName: item['Player'] ?? 'Unknown',
+            position: item['Position'] ?? 'Unknown',
+            goals: item['Goals'] ?? '0',
+            assists: item['Assists'] ?? '0',
+            cleanSheets: item['Clean sheets'] ?? '0',
+            points: item['Points'] ?? '0',
+            price: item['Price'] ?? '0.0',
+            redCards: item['Red Cards'] ?? '0',
+            team: item['Team'] ?? 'Unknown',
+            yellowCards: item['Yellow Cards'] ?? '0',
+          );
+        }).toList();
+
+        players
+            .sort((a, b) => int.parse(b.goals).compareTo(int.parse(a.goals)));
+
+        setState(() {
+          topScorers = players.take(3).toList();
+        });
+      } else {
+        print("No players available.");
+      }
+    } catch (error) {
+      print("Error retrieving data: $error");
+    }
+  }
+
+  Future<void> fetchTopPoints() async {
+    final DatabaseReference _databaseReference =
+        FirebaseDatabase.instance.reference();
+
+    try {
+      final DatabaseEvent event = await _databaseReference.once();
+      final snapshot = event.snapshot;
+
+      if (snapshot.value != null) {
+        final data = snapshot.value as Map<dynamic, dynamic>;
+        final List<dynamic> playersData = data.values.toList();
+
+        List<Player> players = playersData.map((item) {
+          return Player(
+            playerName: item['Player'] ?? 'Unknown',
+            position: item['Position'] ?? 'Unknown',
+            goals: item['Goals'] ?? '0',
+            assists: item['Assists'] ?? '0',
+            cleanSheets: item['Clean sheets'] ?? '0',
+            points: item['Points'] ?? '0',
+            price: item['Price'] ?? '0.0',
+            redCards: item['Red Cards'] ?? '0',
+            team: item['Team'] ?? 'Unknown',
+            yellowCards: item['Yellow Cards'] ?? '0',
+          );
+        }).toList();
+
+        players
+            .sort((a, b) => int.parse(b.points).compareTo(int.parse(a.points)));
+
+        setState(() {
+          topPoints = players.take(3).toList();
+        });
+      } else {
+        print("No players available.");
+      }
+    } catch (error) {
+      print("Error retrieving data: $error");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,98 +140,143 @@ class MainScreen extends StatelessWidget {
           left: 0,
           right: 0,
           bottom: 0,
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text('Points: $userPoints'),
-                  ElevatedButton(
-                    onPressed: () {
-                      _editTeam(context);
-                    },
-                    child: Text('Edit Team'),
-                    style: ElevatedButton.styleFrom(
-                      // primary: Colors.blue,
-                      //onPrimary: Colors.white,
-                      minimumSize: Size(120, 40),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Points: ${widget.userPoints}'),
+                    ElevatedButton(
+                      onPressed: () {
+                        _editTeam(context);
+                      },
+                      child: Text('Edit Team'),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(120, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
-                  ),
-                  Text(countdown),
-                ],
-              ),
-              SizedBox(height: 5),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 185,
+                    Text(widget.countdown),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 185,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey,
+                      ),
+                      child: Center(
+                        child: Text('Score: 0 - 0'),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: 185,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey,
+                      ),
+                      child: Center(
+                        child: Text('Score: 0 - 0'),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 0),
+                InkWell(
+                  onTap: () {
+                    _navigateToMoreMatches(context);
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 30,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: Colors.grey,
                     ),
                     child: Center(
-                      child: Text('Score: 0 - 0'),
+                      child: Text('More Matches'),
                     ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 185,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
-                    ),
-                    child: Center(
-                      child: Text('Score: 0 - 0'),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 0),
-              InkWell(
-                onTap: () {
-                  _navigateToMoreMatches(context);
-                },
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey,
-                  ),
-                  child: Center(
-                    child: Text('More Matches'),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color:
+                            Colors.lightBlue, // Change the color to light blue
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Top Scorers',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            ...topScorers.map((player) {
+                              return Text(
+                                '${player.playerName} - ${player.goals} goals',
+                                style: TextStyle(fontSize: 14),
+                              );
+                            }).toList(),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
+                    Container(
+                      width: 150,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.red, // Change the color to red
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Top Points',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            ...topPoints.map((player) {
+                              return Text(
+                                '${player.playerName} - ${player.points} points',
+                                style: TextStyle(fontSize: 14),
+                              );
+                            }).toList(),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],
