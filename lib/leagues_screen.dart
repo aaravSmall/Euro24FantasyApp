f015675c4eb66a518560163573ebd04a4754b9ba
Aarav Samal <aaravsamal@gmail.com>
2024-05-29 09:29:26 -0400

    predictor work

--- lib/leagues_screen.dart
+++ lib/leagues_screen.dart
@@ -11,13 +11,12 @@ class _TournamentScreenState extends State<TournamentScreen> {
     Group(name: 'Group B', teams: [Team('Spain'), Team('Croatia'), Team('Italy'), Team('Albania')]),
     Group(name: 'Group C', teams: [Team('Slovenia'), Team('Denmark'), Team('Serbia'), Team('England')]),
     Group(name: 'Group D', teams: [Team('Poland'), Team('Netherlands'), Team('Austria'), Team('France')]),
-    Group(name: 'Group E', teams: [Team('Belgium'), Team('Slovakia'), Team('Romania'), Team('Ukraine')]),
-    Group(name: 'Group F', teams: [Team('Turkey'), Team('Georgia'), Team('Portugal'), Team('Czech Republic')]),
   ];
 
-  List<Match> knockoutMatches = [];
-  int currentRoundIndex = 0;
-
+  List<Match> roundOf16Matches = [];
+  List<Match> quarterFinalMatches = [];
+  List<Match> semiFinalMatches = [];
+  Match? finalMatch;
   Map<String, Team?> selectedTeams = {};
 
   @override
@@ -27,37 +26,73 @@ class _TournamentScreenState extends State<TournamentScreen> {
   }
 
   void _initializeKnockoutMatches() {
-    knockoutMatches.clear();
+    _setupRoundOf16();
+    _setupQuarterFinals();
+    _setupSemiFinals();
+    _setupFinal();
+  }
+
+  void _setupRoundOf16() {
+    roundOf16Matches.clear();
     for (int i = 0; i < groups.length - 1; i += 2) {
       Team? team1 = selectedTeams[groups[i].name];
       Team? team2 = selectedTeams[groups[i].name + "_second"];
       Team? team3 = selectedTeams[groups[i + 1].name];
       Team? team4 = selectedTeams[groups[i + 1].name + "_second"];
-      knockoutMatches.add(Match(
-        team1: team1 ?? Team('No Team Selected'),
-        team2: team4 ?? Team('No Team Selected'),
+      roundOf16Matches.add(Match(
+        team1: team1 ?? Team('TBD'),
+        team2: team4 ?? Team('TBD'),
       ));
-      knockoutMatches.add(Match(
-        team1: team3 ?? Team('No Team Selected'),
-        team2: team2 ?? Team('No Team Selected'),
+      roundOf16Matches.add(Match(
+        team1: team3 ?? Team('TBD'),
+        team2: team2 ?? Team('TBD'),
       ));
+    }
+  }
+
+  void _setupQuarterFinals() {
+    quarterFinalMatches.clear();
+    for (int i = 0; i < roundOf16Matches.length; i += 2) {
+      Team? team1 = roundOf16Matches[i].winner;
+      Team? team2 = roundOf16Matches[i + 1].winner;
+      print("Quarters::  Team1:  $team1    Team2: $team2");
+      quarterFinalMatches.add(Match(
+        team1: team1 ?? Team('TBD'),
+        team2: team2 ?? Team('TBD'),
+      ));
+    }
+  }
+
+  void _setupSemiFinals() {
+    semiFinalMatches.clear();
+    for (int i = 0; i < quarterFinalMatches.length; i += 2) {
+      Team? team1 = quarterFinalMatches[i].winner;
+      Team? team2 = quarterFinalMatches[i + 1].winner;
+      print("Semis::  Team1:  $team1    Team2: $team2");
+      semiFinalMatches.add(Match(
+        team1: team1 ?? Team('TBD'),
+        team2: team2 ?? Team('TBD'),
+      ));
+    }
+  }
+
+
+  void _setupFinal() {
+    finalMatch = null;
+    if (semiFinalMatches.length == 2) {
+      finalMatch = Match(
+        team1: semiFinalMatches[0].winner ?? Team('TBD'),
+        team2: semiFinalMatches[1].winner ?? Team('TBD'),
+      );
     }
   }
 
   void _updateKnockoutMatches() {
     setState(() {
-      _initializeKnockoutMatches();
-    });
-  }
-
-  void _progressToNextRound() {
-    setState(() {
-      currentRoundIndex++;
-      if (currentRoundIndex == 3) {
-        // Tournament finished
-        return;
-      }
-      _initializeKnockoutMatches();
+      _setupRoundOf16();
+      _setupQuarterFinals();
+      _setupSemiFinals();
+      _setupFinal();
     });
   }
 
@@ -66,17 +101,27 @@ class _TournamentScreenState extends State<TournamentScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Euro 2020 Predictor')),
       body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Group Stage', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
             ...groups.map((group) => GroupWidget(group: group, selectedTeams: selectedTeams, onUpdate: _updateKnockoutMatches)).toList(),
             SizedBox(height: 20),
-            Text('Knockout Stage', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            ...knockoutMatches.map((match) => MatchWidget(match: match)).toList(),
-            ElevatedButton(
-              onPressed: _progressToNextRound,
-              child: Text('Proceed to Next Round'),
-            ),
+            Text('Quarter Finals', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            ...roundOf16Matches.map((match) => MatchWidget(match: match, onUpdate: _updateKnockoutMatches)).toList(),
+            SizedBox(height: 20),
+            Text('Semi Finals', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            ...quarterFinalMatches.map((match) => MatchWidget(match: match, onUpdate: _updateKnockoutMatches)).toList(),
+            SizedBox(height: 20),
+            Text('Finals', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            ...semiFinalMatches.map((match) => MatchWidget(match: match, onUpdate: _updateKnockoutMatches)).toList(),
+            if (finalMatch != null) ...[
+              SizedBox(height: 20),
+              Text('Final', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              MatchWidget(match: finalMatch!, onUpdate: _updateKnockoutMatches),
+            ],
           ],
         ),
       ),
@@ -111,49 +156,58 @@ class _GroupWidgetState extends State<GroupWidget> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Text(widget.group.name, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          DropdownButtonFormField<Team>(
-            value: firstPlace,
-            items: widget.group.teams.map<DropdownMenuItem<Team>>((Team team) {
-              return DropdownMenuItem<Team>(
-                value: team,
-                child: Text(team.name),
-              );
-            }).toList(),
-            onChanged: (Team? newValue) {
-              setState(() {
-                firstPlace = newValue;
-                widget.selectedTeams[widget.group.name] = newValue;
-                secondPlaceOptions = widget.group.teams.where((team) => team != newValue).toList();
-                if (secondPlaceOptions.contains(secondPlace)) {
-                  secondPlace = null;
-                  widget.selectedTeams['${widget.group.name}_second'] = null;
-                }
-                widget.onUpdate();
-              });
-            },
-            decoration: InputDecoration(labelText: 'First Place'),
-          ),
-          DropdownButtonFormField<Team>(
-            value: secondPlace,
-            items: secondPlaceOptions.map<DropdownMenuItem<Team>>((Team team) {
-              return DropdownMenuItem<Team>(
-                value: team,
-                child: Text(team.name),
-              );
-            }).toList(),
-            onChanged: (Team? newValue) {
-              setState(() {
-                secondPlace = newValue;
-                widget.selectedTeams['${widget.group.name}_second'] = newValue;
-                widget.onUpdate();
-              });
-            },
-            decoration: InputDecoration(labelText: 'Second Place'),
-          ),
-        ],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 4,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.group.name, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            DropdownButtonFormField<Team>(
+              value: firstPlace,
+              items: widget.group.teams.map<DropdownMenuItem<Team>>((Team team) {
+                return DropdownMenuItem<Team>(
+                  value: team,
+                  child: Text(team.name),
+                );
+              }).toList(),
+              onChanged: (Team? newValue) {
+                setState(() {
+                  firstPlace = newValue;
+                  widget.selectedTeams[widget.group.name] = newValue;
+                  secondPlaceOptions = widget.group.teams.where((team) => team != newValue).toList();
+                  if (secondPlaceOptions.contains(secondPlace)) {
+                    secondPlace = null;
+                    widget.selectedTeams['${widget.group.name}_second'] = null;
+                  }
+                  widget.onUpdate();
+                });
+              },
+              decoration: InputDecoration(labelText: 'First Place'),
+            ),
+            SizedBox(height: 10),
+            DropdownButtonFormField<Team>(
+              value: secondPlace,
+              items: secondPlaceOptions.map<DropdownMenuItem<Team>>((Team team) {
+                return DropdownMenuItem<Team>(
+                  value: team,
+                  child: Text(team.name),
+                );
+              }).toList(),
+              onChanged: (Team? newValue) {
+                setState(() {
+                  secondPlace = newValue;
+                  widget.selectedTeams['${widget.group.name}_second'] = newValue;
+                  widget.onUpdate();
+                });
+              },
+              decoration: InputDecoration(labelText: 'Second Place'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -161,8 +215,9 @@ class _GroupWidgetState extends State<GroupWidget> {
 
 class MatchWidget extends StatefulWidget {
   final Match match;
+  final VoidCallback onUpdate;
 
-  MatchWidget({required this.match});
+  MatchWidget({required this.match, required this.onUpdate});
 
   @override
   _MatchWidgetState createState() => _MatchWidgetState();
@@ -172,41 +227,56 @@ class _MatchWidgetState extends State<MatchWidget> {
   Team? selectedWinner;
 
   @override
+  void initState() {
+    super.initState();
+    selectedWinner = widget.match.winner;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(widget.match.team1.name),
-            trailing: Radio<Team>(
-              value: widget.match.team1,
-              groupValue: selectedWinner,
-              onChanged: (Team? value) {
-                setState(() {
-                  selectedWinner = value;
-                  widget.match.winner = value;
-                });
-              },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 4,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(widget.match.team1.name),
+              trailing: Radio<Team>(
+                value: widget.match.team1,
+                groupValue: selectedWinner,
+                onChanged: (Team? value) {
+                  setState(() {
+                    selectedWinner = value;
+                    widget.match.winner = value;
+                    widget.onUpdate();
+                  });
+                },
+              ),
             ),
-          ),
-          ListTile(
-            title: Text(widget.match.team2.name),
-            trailing: Radio<Team>(
-              value: widget.match.team2,
-              groupValue: selectedWinner,
-              onChanged: (Team? value) {
-                setState(() {
-                  selectedWinner = value;
-                  widget.match.winner = value;
-                });
-              },
+            ListTile(
+              title: Text(widget.match.team2.name),
+              trailing: Radio<Team>(
+                value: widget.match.team2,
+                groupValue: selectedWinner,
+                onChanged: (Team? value) {
+                  setState(() {
+                    selectedWinner = value;
+                    widget.match.winner = value;
+                    widget.onUpdate();
+                  });
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+
 
 class Team {
   final String name;
@@ -228,4 +298,3 @@ class Group {
 
   Group({required this.name, required this.teams});
 }
-
